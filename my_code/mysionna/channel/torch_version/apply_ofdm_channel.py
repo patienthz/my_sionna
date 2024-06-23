f425a57096682c56a4f4d50326950d124e0221e5
patienthz <16635905985@163.com>
2024-06-24 01:48:20 +0800

    fix

--- my_code/mysionna/channel/torch_version/apply_ofdm_channel.py
+++ my_code/mysionna/channel/torch_version/apply_ofdm_channel.py
@@ -1,1 +1,28 @@
-from .awgn import AWGN
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from my_code.mysionna.channel.torch_version.awgn import AWGN
+from my_code.mysionna.channel.torch_version.utils import expand_to_rank
+class ApplyOFDMChannel(nn.Module):
+    def __init__(self, add_awgn=True, dtype=torch.complex64):
+        super(ApplyOFDMChannel, self).__init__()
+        self.add_awgn = add_awgn
+        self.dtype = dtype
+        if self.add_awgn:
+            self.awgn = AWGN(dtype=self.dtype)
+
+    def forward(self, inputs):
+        if self.add_awgn:
+            x, h_freq, no = inputs
+        else:
+            x, h_freq = inputs
+
+        # Apply the channel response
+        x = expand_to_rank(x, h_freq.dim(), axis=1)
+        y = torch.sum(torch.sum(h_freq * x, dim=4), dim=3)
+
+        # Add AWGN if requested
+        if self.add_awgn:
+            y = self.awgn((y, no))
+
+        return y
